4dac9a473eca8eb409ec644e76b913829f377e62
JunYoung <dragonjun000204@naver.com>
2026-08-31 23:24:59 +0900

    feat: pretendard 글씨체 의존성 추가

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { VueQueryPlugin } from '@tanstack/vue-query'
 
 import App from './App.vue'
 import router from './router'
+import 'pretendard/dist/web/variable/pretendardvariable-dynamic-subset.css'
 import './style.css'
 
 createApp(App).use(createPinia()).use(router).use(VueQueryPlugin).mount('#app')
